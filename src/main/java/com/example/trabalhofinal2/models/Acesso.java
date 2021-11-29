6c43a704cc1bdbf935ef3a1b5f8c3f5ba506d1d8
Pedro Semensato <semensatopedro@gmail.com>
2021-11-29 00:45:59 -0300

    Atualizado classe Acesso

--- src/main/java/com/example/trabalhofinal2/models/Acesso.java
+++ src/main/java/com/example/trabalhofinal2/models/Acesso.java
@@ -10,10 +10,34 @@ public class Acesso {
 
     private LocalDate dataHora;
 
+    private int cobranca;
+
     public Acesso(Cliente cliente, Entretenimento entretenimento) {
         this.cliente = cliente;
         this.entretenimento = entretenimento;
         this.dataHora = LocalDate.now();
+        this.cobranca = 0;
+        if(entretenimento.defineTipo() == 1 ) this.cobranca = 6;
+        else if(entretenimento.defineTipo() == 4 ) this.cobranca = 4;
+        else this.cobranca = 8;
+    }
+
+    /* public void setCobranca() {
+         int tipo = this.entretenimento.getTipo();
+         // filme
+         if(tipo == 1 ) cobranca = 6;
+             // episodio serie
+         else if(tipo == 4 ) cobranca = 4;
+             // jogo
+         else cobranca = 8;
+     }
+ */
+    public void setCobranca(int cobranca) {
+        this.cobranca = cobranca;
+    }
+
+    public int getCobranca() {
+        return cobranca;
     }
 
     public Cliente getCliente() {
--- src/main/java/com/example/trabalhofinal2/models/CatalogoAcesso.java
+++ src/main/java/com/example/trabalhofinal2/models/CatalogoAcesso.java
@@ -9,37 +9,37 @@ public class CatalogoAcesso {
 
     public ArrayList<Acesso> getCatalogo() { return acessos; }
 
-    //public boolean addAcesso(Acesso acesso){
-//        if(acesso.getCliente().defineTipo() == 3){
-//            acesso.setCobranca(acesso.getCobranca() / 2);
-//
-//            CatalogoUsuarios listaUsuarios = new CatalogoUsuarios();
-//            ArrayList<ClienteIndividual> aux = new ArrayList<>();
-//            for (Usuario value : listaUsuarios.getUsuarios()) {
-//                if (value.defineTipo()==3) {
-//                    aux.add((ClienteIndividual) value);
-//                }
-//            }
-//            ClienteEmpresarial empresanova = null;
-//            for(ClienteIndividual clienteEspecifico : aux){
-//                if(clienteEspecifico.getEmail().equalsIgnoreCase(acesso.getCliente().getEmail())){
-//                    empresanova = clienteEspecifico.getEmpresa();
-//                }
-//            }
-//
-//            Acesso acessoEmpresa = new Acesso(empresanova,acesso.getEntretenimento());
-//            acessoEmpresa.setCobranca(acessoEmpresa.getCobranca() / 2);
-//
-//            acessos.add(acesso);
-//            acessos.add(acessoEmpresa);
-//
-//            return true;
-//        } else if (acesso.getCliente().defineTipo() == 2 || acesso.getCliente().defineTipo() == 1){
-//            acessos.add(acesso);
-//            return true;
-//        }
-//        return false;
-//    }
+    public boolean addAcesso(Acesso acesso){
+        if(acesso.getCliente().defineTipo() == 3){
+            acesso.setCobranca(acesso.getCobranca() / 2);
+
+            CatalogoUsuarios listaUsuarios = new CatalogoUsuarios();
+            ArrayList<ClienteIndividual> aux = new ArrayList<>();
+            for (Usuario value : listaUsuarios.getUsuarios()) {
+                if (value.defineTipo()==3) {
+                    aux.add((ClienteIndividual) value);
+                }
+            }
+            ClienteEmpresarial empresanova = null;
+            for(ClienteIndividual clienteEspecifico : aux){
+                if(clienteEspecifico.getEmail().equalsIgnoreCase(acesso.getCliente().getEmail())){
+                    empresanova = clienteEspecifico.getEmpresa();
+                }
+            }
+
+            Acesso acessoEmpresa = new Acesso(empresanova,acesso.getEntretenimento());
+            acessoEmpresa.setCobranca(acessoEmpresa.getCobranca() / 2);
+
+            acessos.add(acesso);
+            acessos.add(acessoEmpresa);
+
+            return true;
+        } else if (acesso.getCliente().defineTipo() == 2 || acesso.getCliente().defineTipo() == 1){
+            acessos.add(acesso);
+            return true;
+        }
+        return false;
+    }
 
     public ArrayList<Acesso> getAcessosDaqueleMes(int ano, int mes){
         ArrayList<Acesso> resultado = new ArrayList<>();
@@ -51,12 +51,12 @@ public class CatalogoAcesso {
         return resultado;
     }
 
-//    public double cobrancaMensal(int ano, int mes){
-//        double valorFinal = 0;
-//        for (Acesso acesso : getAcessosDaqueleMes(ano, mes)) {
-//            valorFinal = valorFinal + acesso.getCobranca();
-//        }
-//        return valorFinal;
-//    }
+    public double cobrancaMensal(int ano, int mes){
+        double valorFinal = 0;
+        for (Acesso acesso : getAcessosDaqueleMes(ano, mes)) {
+            valorFinal = valorFinal + acesso.getCobranca();
+        }
+        return valorFinal;
+    }
 }
 
